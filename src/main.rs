f79e886896f7826f6ed742209fd8595bc2e2b17f
Abel Stuker <stukerabel@gmail.com>
2025-02-24 00:38:52 +0100

    feat: show contribution graph

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use std::io::Write;
+use termcolor::{ColorSpec, StandardStream, WriteColor};
 
 /*
 Example response from the GitHub API
@@ -48,13 +50,27 @@ struct Week {
     first_day: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct ContributionDay {
     color: String,
     #[serde(rename = "contributionCount")]
     contribution_count: i64,
     date: String,
     weekday: i64,
+}
+
+trait HexToRGB {
+    fn get_rgb(&self) -> (u8, u8, u8);
+}
+
+impl HexToRGB for String {
+    fn get_rgb(&self) -> (u8, u8, u8) {
+        let hex = self.trim_start_matches('#');
+        let r = u8::from_str_radix(&hex[0..2], 16).unwrap();
+        let g = u8::from_str_radix(&hex[2..4], 16).unwrap();
+        let b = u8::from_str_radix(&hex[4..6], 16).unwrap();
+        (r, g, b)
+    }
 }
 
 async fn get_contributions() -> Result<Data, reqwest::Error> {
@@ -101,29 +117,73 @@ async fn get_contributions() -> Result<Data, reqwest::Error> {
     Ok(data)
 }
 
-#[tokio::main]
-async fn main() {
-    let github_contributions = get_contributions().await;
-    // Put every contribution date in a bucket that belongs to a hashmap
-    let mut contributions: HashMap<String, Vec<i64>> = HashMap::new();
-    github_contributions
-        .unwrap()
+async fn split_contributions_in_weeks(contributions: Data) -> Vec<(String, Vec<ContributionDay>)> {
+    let mut contributions_per_week: HashMap<String, Vec<ContributionDay>> = HashMap::new();
+    contributions
         .user
         .contributions_collection
         .contribution_calendar
         .weeks
         .iter()
         .for_each(|week| {
+            // Put contributions in an hashmap with the first day of the week as the key
             week.contribution_days.iter().for_each(|day| {
-                let bucket = contributions.entry(day.date.clone()).or_insert(vec![]);
-                bucket.push(day.contribution_count);
+                contributions_per_week
+                    .entry(week.first_day.clone())
+                    .or_insert_with(Vec::new)
+                    .push(day.clone());
             });
         });
 
-    let mut contributions: Vec<_> = contributions.into_iter().collect();
-    contributions.sort_by(|(date1, _), (date2, _)| date1.cmp(date2));
+    let mut contributions_per_week: Vec<_> = contributions_per_week.into_iter().collect();
+    contributions_per_week.sort_by(|(date1, _), (date2, _)| date1.cmp(date2));
 
-    contributions.iter().for_each(|(date, count)| {
-        println!("Date: {}, Count: {:?}", date, count);
-    });
+    contributions_per_week
+}
+
+async fn convert_weeks_to_rows(
+    contributions_per_week: Vec<(String, Vec<ContributionDay>)>,
+) -> Vec<Vec<ContributionDay>> {
+    // All first days of the week go in the first row, second days in the second row, etc.
+    let mut contributions_per_row: Vec<Vec<ContributionDay>> = vec![vec![]; 7];
+    for (_, contributions) in contributions_per_week {
+        for (i, contribution) in contributions.iter().enumerate() {
+            contributions_per_row[i].push(contribution.clone());
+        }
+    }
+
+    contributions_per_row
+}
+
+async fn print_contribution_graph(contributions_per_row: Vec<Vec<ContributionDay>>) {
+    let mut stdout = StandardStream::stdout(termcolor::ColorChoice::Always);
+    for row in contributions_per_row {
+        for contribution in row {
+            let rgb = contribution.color.get_rgb();
+            stdout
+                .set_color(
+                    ColorSpec::new().set_fg(Some(termcolor::Color::Rgb(rgb.0, rgb.1, rgb.2))),
+                )
+                .expect("Failed to set color");
+
+            if rgb.0 > 200 && rgb.1 > 200 && rgb.2 > 200 {
+                stdout
+                    .set_color(ColorSpec::new().set_dimmed(true))
+                    .expect("Failed to set dimmed");
+            }
+            write!(&mut stdout, "■ ").expect("Failed to write to stdout");
+        }
+        write!(stdout, "\n").expect("Failed to write to stdout");
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let github_contributions = get_contributions()
+        .await
+        .expect("Failed to get contributions");
+
+    let github_contributions_per_week = split_contributions_in_weeks(github_contributions).await;
+    let github_contributions_per_row = convert_weeks_to_rows(github_contributions_per_week).await;
+    print_contribution_graph(github_contributions_per_row).await;
 }
