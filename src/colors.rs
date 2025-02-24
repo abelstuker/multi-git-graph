d3a9365ee4f2021546fae20d81d89b086a7ebce2
Abel Stuker <stukerabel@gmail.com>
2025-02-24 10:59:46 +0100

    feat: color options

---
+++ src/colors.rs
@@ -0,0 +1,27 @@
+// Source: https://github.com/williambelle/github-contribution-color-graph
+
+pub const GITHUB_COLORS: [&str; 5] = ["#eeeeee", "#216e39", "#30a14e", "#40c463", "#9be9a8"];
+pub const HALLOWEEN_COLORS: [&str; 5] = ["#eeeeee", "#04001b", "#ff9711", "#ffc722", "#fdf156"];
+
+pub const AMBER_COLORS: [&str; 5] = ["#eeeeee", "#ff6f00", "#ffb300", "#ffd54f", "#ffecb3"];
+pub const BLUE_COLORS: [&str; 5] = ["#eeeeee", "#0d47a1", "#1e88e5", "#64b5f6", "#bbdefb"];
+pub const BLUEGREY_COLORS: [&str; 5] = ["#eeeeee", "#263238", "#546e7a", "#90a4ae", "#cfd8dc"];
+pub const BROWN_COLORS: [&str; 5] = ["#eeeeee", "#3e2723", "#6d4c41", "#a1887f", "#d7ccc8"];
+pub const CYAN_COLORS: [&str; 5] = ["#eeeeee", "#006064", "#00acc1", "#4dd0e1", "#b2ebf2"];
+pub const DEEPORANGE_COLORS: [&str; 5] = ["#eeeeee", "#bf360c", "#f4511e", "#ff8a65", "#ffccbc"];
+pub const DEEPPURPLE_COLORS: [&str; 5] = ["#eeeeee", "#311b92", "#5e35b1", "#9575cd", "#d1c4e9"];
+pub const GREEN_COLORS: [&str; 5] = ["#eeeeee", "#1b5e20", "#43a047", "#81c784", "#c8e6c9"];
+pub const GREY_COLORS: [&str; 5] = ["#eeeeee", "#212121", "#616161", "#9e9e9e", "#e0e0e0"];
+pub const INDIGO_COLORS: [&str; 5] = ["#eeeeee", "#1a237e", "#3949ab", "#7986cb", "#c5cae9"];
+pub const LIGHTBLUE_COLORS: [&str; 5] = ["#eeeeee", "#01579b", "#039be5", "#4fc3f7", "#b3e5fc"];
+pub const LIGHTGREEN_COLORS: [&str; 5] = ["#eeeeee", "#33691e", "#7cb342", "#aed581", "#dcedc8"];
+pub const LIME_COLORS: [&str; 5] = ["#eeeeee", "#827717", "#c0ca33", "#dce775", "#f0f4c3"];
+pub const ORANGE_COLORS: [&str; 5] = ["#eeeeee", "#e65100", "#fb8c00", "#ffb74d", "#ffe0b2"];
+pub const PINK_COLORS: [&str; 5] = ["#eeeeee", "#880e4f", "#e91e63", "#f06292", "#f8bbd0"];
+pub const PURPLE_COLORS: [&str; 5] = ["#eeeeee", "#4a148c", "#8e24aa", "#ba68c8", "#e1bee7"];
+pub const RED_COLORS: [&str; 5] = ["#eeeeee", "#b71c1c", "#e53935", "#e57373", "#ffcdd2"];
+pub const TEAL_COLORS: [&str; 5] = ["#eeeeee", "#004d40", "#00897b", "#4db6ac", "#b2dfdb"];
+pub const YELLOWMD_COLORS: [&str; 5] = ["#eeeeee", "#f57f17", "#ffd835", "#fff176", "#fff9c4"];
+
+pub const MOON_COLORS: [&str; 5] = ["#eeeeee", "#4f2266", "#48009a", "#00a1f3", "#6bcdff"];
+pub const PSYCHEDELIC_COLORS: [&str; 5] = ["#eeeeee", "#ff00ab", "#fa3fbc", "#fb6dcc", "#faafe1"];
